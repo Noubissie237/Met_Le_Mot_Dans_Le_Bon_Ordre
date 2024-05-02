0de57caa58da774bdc116f6676c11bd7a33af5f9
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-05-02 09:28:03 +0200

    Ajoutez des fichiers projet.

---
+++ Met_Le_Mot_Dans_Le_Bon_Ordre/NK.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NKbibliotheque
+{
+    static internal class NK
+    {
+
+        public static char DemanderUneLettreDans(string message, List<char> chars)
+        {
+            char letter = DemanderUneLettre(message);
+
+            if (!chars.Contains(letter))
+            {
+                return DemanderUneLettreDans("ERRUER, Vous devez choisir une lettre parmis celles proposées : ", chars);
+            }
+            return letter;
+        }
+        public static char DemanderUneLettre(string message)
+        {
+            string chiffres = "0,1,2,3,4,5,6,7,8,9";
+            string alphabetMin = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+            string alphabetMax = alphabetMin.ToString().ToUpper();
+            string alphabet = alphabetMin + alphabetMax;
+
+            Console.Write(message);
+            string lettre = Console.ReadLine();
+
+            //  Cas où l'utilisateur entre plusieurs lettre (un mot)
+            if (lettre.Length > 1)
+                return DemanderUneLettre("\nERREUR : Veuillez entrer une seule lettre : ");
+
+            if (lettre.Length == 0)
+                return DemanderUneLettre("\nERREUR : Vous devez entrer obligatoirement une lettre : ");
+
+            if (chiffres.Contains(lettre))
+                return DemanderUneLettre("\nERREUR : Vous ne pouvez entrer un chiffre, veuillez entrer une lettre : ");
+
+            if (!alphabet.Contains(lettre))
+                return DemanderUneLettre("\nERREUR : Caractere inconnu, Veuillez entrer une lettre de l'alphabet : ");
+
+            lettre = lettre.ToUpper();
+
+            return lettre[0];
+            
+        }
+        public static int DemanderNombrePositifNonNull(string message)
+        {
+            int nombre = DemanderNombre(message);
+            if (nombre > 0)
+                return nombre;
+            return DemanderNombrePositifNonNull("\nLe nombre doit etre superieur a zéro (0) : ");
+        }
+        public static int DemanderNombreEntre(string message, int min, int max)
+        {
+            int nombre = DemanderNombre(message);
+            if (nombre < min || nombre > max)
+                return DemanderNombreEntre($"\nLe nombre doit être compris entre {min} et {max} : ", min, max);
+            return nombre;
+        }
+        public static int DemanderNombre(string message)
+        {
+            Console.Write(message);
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                return number;
+            }
+            catch
+            {
+                return DemanderNombre("\nErreur, vous devez entrer un entier : ");
+            }
+        }
+    }
+}
---
+++ Met_Le_Mot_Dans_Le_Bon_Ordre/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using NKbibliotheque;
+using Jeux;
+
+namespace Met_Le_Mot_Dans_Le_Bon_Ordre
+{
+    public class Program
+    {
+        
+        
+        // Fonction permettant d'obtenir le mot dans le mauvais ordre
+        public static string getBadWord(string correctWord)
+        {
+            var correctWordList = new List<char>();
+            var badWordAtList = new List<char>();
+
+            for (int i = 0; i < correctWord.Length; i++)
+                correctWordList.Add(correctWord[i]);
+
+            while (correctWordList.Count > 0)
+            {
+                var rand = new Random();
+                int position = rand.Next(correctWordList.Count);
+                badWordAtList.Add(correctWordList[position]);
+                correctWordList.RemoveAt(position);
+            }
+
+            return String.Join("", badWordAtList);
+        }
+
+        // Fonction permettant de charger les mots a partir du fichier file
+        public static string[] getWords(string file)
+        {
+            try
+            {
+                return File.ReadAllLines(file);
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("Echec d'ouverture du fichier : "+e.Message);
+            }
+            return null;
+        }
+        
+        // Fonction permettant de se rassurer que le nombre d'occurence d'un caractere ne deborde pas
+        public static char CheckAvailability(char tmpcaracter, List<char> awaited, List<char> check)
+        {
+            check.Add(tmpcaracter);
+
+            int nbNeed, nbEnter;
+
+            nbNeed = awaited.Where(x => x == tmpcaracter).Count();
+            nbEnter = check.Where(y => y == tmpcaracter).Count();
+
+            if (nbEnter > nbNeed)
+            {
+                check.RemoveAt(check.Count - 1);
+                tmpcaracter = NK.DemanderUneLettreDans($"Vous ne pouvez pas entrer plus de {nbNeed} lettre(s) '{tmpcaracter}' : ", awaited);
+                return CheckAvailability(tmpcaracter, awaited, check);
+            }
+
+            return tmpcaracter;
+        }
+
+        // Fonction permettant de verifier que les mots sont identiques
+        public static bool CheckWords(string word, char[] userWord)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != userWord[i])
+                    return false;
+            }
+            return true;
+        }
+
+        // Fonction pour reinitialiser les valeurs enrégistrées
+        public static void Vider(char[] list1, List<char> list2)
+        {
+            for(int i = 0;i< list1.Length;i++)
+            {
+                list1[i] = '\0';
+            }
+
+            list2.Clear();
+        }
+
+        // Fonction permettant de ranger le mot dans le bon ordre
+        public static void OrderTheWord(string correctWord, string badWord, int NB_VIE)
+        {
+            var listOfUserCaracter = new char[badWord.Length];
+            var listForCheck = new List<char>();
+
+            while (NB_VIE > 0)
+            {
+                for (int i = 0;i < correctWord.Length;i++)
+                {
+                    Console.WriteLine(Bonhomme.PENDU[3 - NB_VIE]);
+                    Console.WriteLine("Trouvez le bon ordre du mot : " + badWord);
+                    printWord(badWord, listOfUserCaracter);
+                    Console.WriteLine("\n");
+                    char tmpcaracter = NK.DemanderUneLettreDans("Entrez une lettre : ", string.Join(",", badWord).ToList());
+                    char caracter = CheckAvailability(tmpcaracter, String.Join(",", correctWord).ToList(), listForCheck);
+                    Console.Clear();
+                    listOfUserCaracter[i] = caracter;
+                }
+
+                if(CheckWords(correctWord, listOfUserCaracter))
+                {
+                    Console.WriteLine(Bonhomme.PENDU[3 - NB_VIE]);
+                    printWord(badWord, listOfUserCaracter);
+                    Console.WriteLine();
+                    Console.WriteLine("\nGAGNE !!!");
+                    break;
+                }
+
+                Console.WriteLine("MAUVAIS ORDRE !");
+                NB_VIE -= 1;
+                Console.WriteLine("Vie(s) restante(s) : "+NB_VIE);
+                Vider(listOfUserCaracter, listForCheck);                
+            }
+
+            if (NB_VIE == 0)
+            {
+                Console.WriteLine(Bonhomme.PENDU[3 - NB_VIE]);
+                printWord(badWord, listOfUserCaracter);
+                Console.WriteLine();
+                Console.WriteLine("\nPERDU, le mot etaits : "+correctWord);
+            }
+
+        }
+
+        // Fonction permettant d'afficher le mot
+        public static void printWord(string word, char[] userCaracters)
+        {
+            for (int i = 0;i < word.Length;i++)
+            {
+                if (userCaracters[i] != 0)
+                    Console.Write(userCaracters[i]+" ");
+                else
+                    Console.Write("_ ");
+            }
+        }
+
+        // Programme principale
+        public static void Main(string[] args)
+        {
+            var words = getWords("bd_words.txt");
+            int NB_VIES = 3;
+
+            if (words != null)
+            {
+                if (words.Length == 0)
+                {
+                    Console.WriteLine("Le fichier est vide !");
+                }
+                else
+                {
+                    while (true)
+                    {
+                        var rand = new Random();
+                        string word = words[rand.Next(words.Length)].ToUpper().Trim();
+                        string badWord = getBadWord(word);
+
+                        OrderTheWord(word, badWord, NB_VIES);
+
+                        Console.WriteLine();
+
+                        char choix = NK.DemanderUneLettreDans("Vous les vous rejouer ? (o/n) ", new List<char> { 'O', 'N' });
+                        if (choix.ToString().ToLower() == "n")
+                        {
+                            Console.WriteLine("A bientôt !");
+                            break;
+                        }
+                        Console.Clear();
+
+                    }
+                }
+            }
+            
+        }
+    }
+}
